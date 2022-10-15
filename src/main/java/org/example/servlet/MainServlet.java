58e78e42f0e2d9ad4346b82564013c9ad7eee802
moshn <moshna.ivan@gmail.com>
2022-10-15 19:17:19 +0700

    added some logs

--- src/main/java/org/example/servlet/MainServlet.java
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -11,20 +11,23 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
+        log("This is init method");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("This is service method enter\n");
         super.service(req, resp);
+        resp.getWriter().write("This is service method exit\n");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.getWriter().write("This is doGet method\n");
     }
 
     @Override
     public void destroy() {
-
+        log("This is destroy method");
     }
 }
