eb94c09df7010880cc0149d3ae77e4bbd598b015
moshn <moshna.ivan@gmail.com>
2022-10-15 17:42:49 +0700

    first servlet

--- src/main/java/org/example/servlet/MainServlet.java
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -1,6 +1,30 @@
 package org.example.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
